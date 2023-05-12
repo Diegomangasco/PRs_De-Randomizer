e9e536add575ff53157cb9adc29b623646f53781
Diego Gasco <diego.gasco99@gmail.com>
2023-05-12 11:14:26 +0200

    Bug fixed for labels

--- neural_network/load_data.py
+++ neural_network/load_data.py
@@ -24,7 +24,7 @@ def load_data(file_path: str, batch_size: int):
     train_data = inputs[:int(0.8*len(inputs))][:]
     train_labels = labels[:int(0.8*len(labels))]
     validate_data = inputs[int(0.8*len(inputs)):][:]
-    validate_labels = inputs[int(0.8*len(labels)):]
+    validate_labels = labels[int(0.8*len(labels)):]
     train_data = torch.tensor(train_data, dtype=torch.float32)
     validate_data = torch.tensor(validate_data, dtype=torch.float32)
     train_dataset = CustomDataset(train_data, train_labels)
